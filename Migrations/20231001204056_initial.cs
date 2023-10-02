7b64cd40dec8f5a1133af339f04895b137bc488a
Claudinei José Santos <claudineibitta@gmail.com>
2023-10-01 23:46:33 -0300

    feat: adicionando migrations

---
+++ Migrations/20231001204056_initial.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartSchool.WebAPI.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: true),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: true),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Disciplines",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    TeacherId = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Disciplines", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Disciplines_Teachers_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Teachers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentsDisciplines",
+                columns: table => new
+                {
+                    StudentId = table.Column<long>(type: "INTEGER", nullable: false),
+                    DisciplineId = table.Column<long>(type: "INTEGER", nullable: false),
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentsDisciplines", x => new { x.StudentId, x.DisciplineId });
+                    table.ForeignKey(
+                        name: "FK_StudentsDisciplines_Disciplines_DisciplineId",
+                        column: x => x.DisciplineId,
+                        principalTable: "Disciplines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentsDisciplines_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Disciplines_TeacherId",
+                table: "Disciplines",
+                column: "TeacherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentsDisciplines_DisciplineId",
+                table: "StudentsDisciplines",
+                column: "DisciplineId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentsDisciplines");
+
+            migrationBuilder.DropTable(
+                name: "Disciplines");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
---
+++ Migrations/20231001210552_charge-initial.cs
@@ -0,0 +1,415 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartSchool.WebAPI.Migrations
+{
+    public partial class chargeinitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Email", "FirstName", "LastName" },
+                values: new object[] { 1L, "33225555", "Marta", "Kent" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Email", "FirstName", "LastName" },
+                values: new object[] { 2L, "3354288", "Paula", "Isabela" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Email", "FirstName", "LastName" },
+                values: new object[] { 3L, "55668899", "Laura", "Antonia" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Email", "FirstName", "LastName" },
+                values: new object[] { 4L, "6565659", "Luiza", "Maria" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Email", "FirstName", "LastName" },
+                values: new object[] { 5L, "565685415", "Lucas", "Machado" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Email", "FirstName", "LastName" },
+                values: new object[] { 6L, "456454545", "Pedro", "Alvares" });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Email", "FirstName", "LastName" },
+                values: new object[] { 7L, "9874512", "Paulo", "José" });
+
+            migrationBuilder.InsertData(
+                table: "Teachers",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 1L, "Lauro", "Fernandes" });
+
+            migrationBuilder.InsertData(
+                table: "Teachers",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 2L, "Roberto", "Solto" });
+
+            migrationBuilder.InsertData(
+                table: "Teachers",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 3L, "Ronaldo", "Rabelo" });
+
+            migrationBuilder.InsertData(
+                table: "Teachers",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 4L, "Rodrigo", "Meneses" });
+
+            migrationBuilder.InsertData(
+                table: "Teachers",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[] { 5L, "Alexandre", "Miguel" });
+
+            migrationBuilder.InsertData(
+                table: "Disciplines",
+                columns: new[] { "Id", "Name", "TeacherId" },
+                values: new object[] { 1L, "Matemática", 1L });
+
+            migrationBuilder.InsertData(
+                table: "Disciplines",
+                columns: new[] { "Id", "Name", "TeacherId" },
+                values: new object[] { 2L, "Física", 2L });
+
+            migrationBuilder.InsertData(
+                table: "Disciplines",
+                columns: new[] { "Id", "Name", "TeacherId" },
+                values: new object[] { 3L, "Português", 3L });
+
+            migrationBuilder.InsertData(
+                table: "Disciplines",
+                columns: new[] { "Id", "Name", "TeacherId" },
+                values: new object[] { 4L, "Inglês", 4L });
+
+            migrationBuilder.InsertData(
+                table: "Disciplines",
+                columns: new[] { "Id", "Name", "TeacherId" },
+                values: new object[] { 5L, "Programação", 5L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 2L, 1L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 4L, 1L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 5L, 1L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 1L, 2L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 2L, 2L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 5L, 2L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 1L, 3L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 2L, 3L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 3L, 3L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 1L, 4L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 4L, 4L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 5L, 4L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 4L, 5L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 5L, 5L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 1L, 6L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 2L, 6L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 3L, 6L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 4L, 6L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 1L, 7L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 2L, 7L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 3L, 7L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 4L, 7L, 0L });
+
+            migrationBuilder.InsertData(
+                table: "StudentsDisciplines",
+                columns: new[] { "DisciplineId", "StudentId", "Id" },
+                values: new object[] { 5L, 7L, 0L });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 2L, 1L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 4L, 1L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 5L, 1L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 1L, 2L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 2L, 2L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 5L, 2L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 1L, 3L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 2L, 3L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 3L, 3L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 1L, 4L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 4L, 4L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 5L, 4L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 4L, 5L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 5L, 5L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 1L, 6L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 2L, 6L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 3L, 6L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 4L, 6L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 1L, 7L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 2L, 7L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 3L, 7L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 4L, 7L });
+
+            migrationBuilder.DeleteData(
+                table: "StudentsDisciplines",
+                keyColumns: new[] { "DisciplineId", "StudentId" },
+                keyValues: new object[] { 5L, 7L });
+
+            migrationBuilder.DeleteData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 6L);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 7L);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "Teachers",
+                keyColumn: "Id",
+                keyValue: 5L);
+        }
+    }
+}
